65e52150ed3b70572f384da7c69b53e8d2214dd9
KacperParzych <157218473+KacperParzych@users.noreply.github.com>
2024-10-12 19:36:41 +0200

    Add files via upload

---
+++ Zestaw1.java
@@ -0,0 +1,30 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //1
+        Object w = 23.0+76;
+        System.out.println(w.getClass());
+        w=41*2.0+3;
+        System.out.println(w.getClass());
+        w = 5-33;
+        System.out.println(w.getClass());
+        w = 109%3;
+        System.out.println(w.getClass());
+        w=50/2;
+        System.out.println(w.getClass());
+        w=4|2;
+        System.out.println(w.getClass());
+        w=3^5;
+        System.out.println(w.getClass());
+        w=7&9;
+        System.out.println(w.getClass());
+        //2
+        Object w1 = (Math.sqrt(7)-1)/2+Math.pow(3,3)%2;
+        System.out.println((int)w1);
+        Object w2 = ()
+
+
+
+
+    }
+}
